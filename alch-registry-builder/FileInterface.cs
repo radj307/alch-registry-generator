1c5ad245625b66401850b7856ab47952bc426219
radj307 <radj307@gmail.com>
2021-12-15 16:17:09 -0500

    fix some bugs

--- alch-registry-builder/FileInterface.cs
+++ alch-registry-builder/FileInterface.cs
@@ -6,26 +6,39 @@ namespace Mutagen.alch_registry_builder
 {
     public class FileInterface
     {
-        public FileInterface(string filepath)
+        public FileInterface(string filepath, bool printToConsole = false)
         {
-            File = new StreamWriter(filepath);
+            if (File.Exists(filepath))
+                File.Delete(filepath);
+            FileHandle = new StreamWriter(filepath, true);
+            PrintToConsole = printToConsole;
         }
 
-        public StreamWriter File;
+        public StreamWriter FileHandle;
+        public bool PrintToConsole;
 
         public void WriteLine(string data)
         {
-            Console.WriteLine();
-            File.WriteLineAsync(data);
+            FileHandle.WriteLine(data);
+            if (PrintToConsole)
+                Console.WriteLine(data);
         }
         // Write a list of strings to file, each element is one line.
-        public void Write(ref List<string> data)
+        public void Write(List<string> data)
         {
             foreach (var line in data)
-            {
-                File.WriteLine(line);
-                Console.WriteLine(line);
-            }
+                WriteLine(line);
+        }
+
+        public void Append(List<string> data)
+        {
+            foreach (var line in data)
+                WriteLine(line);
+        }
+
+        public void Close()
+        {
+            FileHandle.Close();
         }
     }
 }
--- alch-registry-builder/Program.cs
+++ alch-registry-builder/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Mutagen.alch_registry_builder
 {
@@ -17,34 +18,41 @@ namespace Mutagen.alch_registry_builder
 
         static void Main(string[] args)
         {
-            // Initialize GameEnvironment
-            var env = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimSE);
-
-            // Set the output directory
-            string out_path = GetOutPath("alch.ingredients");
-
-            // Initialize FileInterface
-            FileInterface IO = new(out_path);
-            // Initialize output data list
-            List<string> data = new();
-
-            Console.WriteLine($"\nConfiguration\n");
-            Console.WriteLine($"\tOutput:\t{out_path}");
-
-            Console.WriteLine("\n\n=== Beginning Process. ===\nGetting a list of all ingredients...\n");
-
-            int count = 0;
-            foreach (var ingr in env.LoadOrder.PriorityOrder.Ingredient().WinningOverrides().Where(i => i.EditorID != null))
+            try
             {
-                data.AddRange(Formatter.FormatIngredient(ingr, env));
-                ++count;
+                // Initialize GameEnvironment
+                var env = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimSE);
+
+                // Set the output directory
+                string out_path = GetOutPath("alch.ingredients");
+
+                // Initialize FileInterface
+                FileInterface O = new(out_path);
+
+                Console.WriteLine($"\nConfiguration\n");
+                Console.WriteLine($"\tOutput:\t{out_path}");
+
+                Console.WriteLine("\n\n=== Beginning Process. ===\n\nGetting a list of all ingredients...\n");
+
+                int count = 0;
+                foreach (var ingr in env.LoadOrder.PriorityOrder.Ingredient().WinningOverrides().Where(i => i.EditorID != null))
+                {
+                    Console.WriteLine($"[{++count}]\tFound ingredient \"{ingr.Name}\"");
+                    O.Write(Formatter.FormatIngredient(ingr, env));
+                }
+
+                Console.WriteLine("\n\n=== Process Completed. ===\n");
+
+                O.Close();
+
+                Console.WriteLine($"Successfully wrote {count} ingredient{(count > 1 ? "s" : "")} to file:\n\"{out_path}\"\n");
+                Thread.Sleep(2000);
             }
-
-            Console.WriteLine("\n\n=== Process Completed. ===\nWriting to file...\n");
-
-            IO.Write(ref data);
-
-            Console.WriteLine($"Successfully wrote {count} ingredient{(count > 1 ? "s" : "")} to \"{out_path}\"\n");
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
         }
     }
 }
